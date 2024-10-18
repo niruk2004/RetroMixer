5841f58907fb04d89d95604e8ce8181e7dc7b01b
niruk2004 <niranjank2929@gmail.com>
2024-10-18 23:01:25 +0530

    deleted right container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import classes from "./HomePage.module.css";
 import LeftContainer from "../components/HomeContainers/LeftContainer";
-import RightContainer from "../components/HomeContainers/RightContainer";
+
 export default function HomePage() {
   return (
     <div className={classes.mainContainer}>
